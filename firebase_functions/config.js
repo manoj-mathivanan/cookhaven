e86fd070bad798490617f0800e487d29bfc6d968
Manoj Mathivanan <manoj.mathivanan@sap.com>
2020-12-11 13:26:58 +0530

    initial structure

---
+++ firebase_functions/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    stripe_config_test: {
+        PUBLISHABLE_KEY: 'STRIPE_PUBLISHABLE_KEY',
+        SECRET_KEY: 'STRIPE_SECRET_KEY'
+    },
+    razorpay_config:{
+        KEY_ID:'rzp_test_3tZ6SxufwcV8t8',
+        KEY_SECRET:'cyEwsbXIfXSaDBJ5EOaC4yoV'
+    },
+    trending_prod_no: 5,
+    low_inventory_limit: 20,
+    currency: 'INR',
+    razorpay_auth_token: 'YOUR_AUTHORIZATION_TOKEN_HERE'
+}
---
+++ grocery_store/android/app/src/main/java/com/cookhaven/grocery_store/MainActivity.java
@@ -0,0 +1,12 @@
+package com.cookhaven.grocery_store;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+  // @Override
+  // public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+  //   super.configureFlutterEngine(flutterEngine);
+
+  //   GeneratedPluginRegistrant.registerWith(flutterEngine);
+  // }
+}
